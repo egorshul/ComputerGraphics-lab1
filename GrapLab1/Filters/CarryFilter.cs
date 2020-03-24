cdaf07bc5674dff3b92f1230aca72b092c40d193
Oleg <olejka647@gmail.com>
2020-03-24 16:44:22 +0300

    f

---
+++ GrapLab1/Filters/CarryFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace GrapLab1
+{
+    class CarryFilter : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int k, l;
+            Color result;
+
+            if (x >= sourceImage.Width - 50)
+                return result = Color.FromArgb(0, 0, 0);
+
+            k = Clamp(x + 50, 0, sourceImage.Width - 1);
+            l = Clamp(y, 0, sourceImage.Height - 1);
+            Color sourceColor = sourceImage.GetPixel(k, l);
+            result = Color.FromArgb(sourceColor.R, sourceColor.G, sourceColor.B);
+
+            return result;
+        }
+    }
+}
---
+++ GrapLab1/Filters/WavesFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace GrapLab1
+{
+    class WavesFilter : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourseImage, int x, int y)
+        {
+            int k = (int)(x + 20 * Math.Sin(2.0 * Math.PI * y / 60.0));
+            int l = y;
+
+            if ((k < sourseImage.Width - 1) && (l < sourseImage.Height - 1) && (k > 0) && (l > 0))
+            {
+                Color resultColor = sourseImage.GetPixel(k, l);
+                return resultColor;
+            }
+            return Color.Transparent;
+        }
+    }
+}
